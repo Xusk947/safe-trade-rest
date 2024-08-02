c99df78be34eae8277ad3b0d8930deef911cc954
Xusk947 <xusk947@gmail.com>
2024-08-03 01:41:29 +0500

    Added NFT support

--- src/crypto/controllers/crypto/crypto.controller.ts
+++ src/crypto/controllers/crypto/crypto.controller.ts
@@ -1,16 +1,24 @@
 import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { TonApiClient } from 'src/crypto/client/tonApiClient';
+import { CryptoService } from 'src/crypto/services/crypto/crypto.service';
 
 @Controller('crypto')
 export class CryptoController {
     private logger = new Logger(CryptoController.name)
 
+    constructor(
+        private cryptoService: CryptoService
+    ) { }
+
     @Get("account/:id/jetton") 
     async getAccount(@Param('id') id: string) {
-        this.logger.log(`Get account ${id} jettons balances`)    
-        let response = await TonApiClient.accounts.getAccountJettonsBalances(id)
+        this.logger.log(`Get account ${id} jettons`)    
+        return this.cryptoService.getAccountJettons(id)
+    }
 
-
-        return response
+    @Get("account/:id/nfts")
+    async getAccountNfts(@Param('id') id: string) {
+        this.logger.log(`Get account ${id} nft items`)    
+        return this.cryptoService.getAccountNfts(id)
     }
 }
--- src/crypto/services/crypto/crypto.service.ts
+++ src/crypto/services/crypto/crypto.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { TonApiClient } from 'src/crypto/client/tonApiClient';
 
 @Injectable()
-export class CryptoService {}
+export class CryptoService {
+    private logger = new Logger(CryptoService.name)
+    
+    async getAccountJettons(id: string) {
+        return await TonApiClient.accounts.getAccountJettonsBalances(id)
+    }
+
+    async getAccountNfts(id: string) {
+        return await TonApiClient.accounts.getAccountNftItems(id)
+    }
+}
