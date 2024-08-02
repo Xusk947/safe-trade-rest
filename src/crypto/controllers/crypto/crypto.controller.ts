c37a067f2b5e55abacda494926843094e568f669
Xusk947 <xusk947@gmail.com>
2024-08-03 01:23:03 +0500

    Logger

--- src/crypto/controllers/crypto/crypto.controller.ts
+++ src/crypto/controllers/crypto/crypto.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { TonApiClient } from 'src/crypto/client/tonApiClient';
 
 @Controller('crypto')
 export class CryptoController {
+    private logger = new Logger(CryptoController.name)
+
     @Get("account/:id/jetton") 
     async getAccount(@Param('id') id: string) {
-        return await TonApiClient.accounts.getAccountJettonsBalances(id)
+        let response = await TonApiClient.accounts.getAccountJettonsBalances(id)
+
+        this.logger.log(`Get account ${id} jettons balances`)    
+
+        return response.balances
     }
 }
