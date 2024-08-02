abc82560b3818d6bd64c63a43d33fff343201367
Xusk947 <xusk947@gmail.com>
2024-08-02 16:31:03 +0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     console.log(process.env.PORT || 3000)
-    const app = await NestFactory.create(
-        AppModule,
-    );
+    
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+    });
+
+    app.enableCors({
+        allowedHeaders: ['content-type', 'authorization'],
+        origin: true,
+        methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD",
+        credentials: true,
+    })
+
     await app.listen(process.env.PORT || 3000);
 }
 
